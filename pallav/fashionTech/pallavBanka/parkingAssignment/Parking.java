1c14fa762ee977f22781323da72432c2f51d3531
pallav2899 <pallavbanka28@gmail.com>
2019-05-29 20:14:27 +0530

    Parking Assignment Test 1

--- pallav/fashionTech/pallavBanka/parkingAssignment/Parking.java
+++ pallav/fashionTech/pallavBanka/parkingAssignment/Parking.java
@@ -1,2 +1,137 @@
 package com.fashionTech.pallavBanka.parkingAssignment;
 
+import java.util.Date;
+
+class vehicle{
+    void drive(){
+
+    }
+}
+class twoWheelers extends vehicle{
+    short floor;
+    /*int slotid[];
+    void slotid(){
+        for(int i=0;i<30;i++){
+            this.slotid[i]=i+1;
+        }
+    }*/
+    void floor(short floor) {
+        this.floor=floor;
+        if (floor == 1 || floor == 2 || floor==5){
+            System.out.println("You are on floor:" + floor + "This floor is for two wheeler.");
+        } else
+            System.out.println("You are on the wrong floor.");
+    }
+    void drive(){
+
+    }
+}
+class scooties extends twoWheelers{
+    int regno;
+    Date inTime;
+    int slotid;
+    void drive(){
+        if(slotid>30) {
+            System.out.println("This floor is full");
+        }else
+            System.out.println("Your floor is:" + floor + "Your Slot Id is:" + slotid);
+    }
+}
+class cycle extends twoWheelers{
+    int regno;
+    Date inTime;
+    int slotid;
+    void drive(){
+        if(slotid>30) {
+            System.out.println("This floor is full");
+        }else
+            System.out.println("Your floor is:" + floor + "Your Slot Id is:" + slotid);
+    }
+}
+class sportsBike extends twoWheelers{
+    int regno;
+    Date inTime;
+    int slotid;
+    void drive(){
+        if(slotid>30) {
+            System.out.println("This floor is full");
+        }else
+            System.out.println("Your floor is:" + floor + "Your Slot Id is:" + slotid);
+    }
+}
+class cruiserBike extends twoWheelers{
+    int regno;
+    Date inTime;
+    int slotid;
+    void drive(){
+        if(slotid>30) {
+            System.out.println("This floor is full");
+        }else
+            System.out.println("Your floor is:" + floor + "Your Slot Id is:" + slotid);
+    }
+}
+class fourWheelers extends vehicle{
+    short floor;
+    void floor(short floor) {
+        this.floor=floor;
+        if (floor == 3 || floor == 4 || floor==6){
+            System.out.println("You are on floor:" + floor + "This floor is for two wheeler.");
+        } else
+            System.out.println("You are on the wrong floor.");
+    }
+    void drive(){
+
+    }
+}
+class hatchBack extends fourWheelers{
+    int regno;
+    Date inTime;
+    int slotid;
+    void drive(){
+        if(slotid>30) {
+            System.out.println("This floor is full");
+        }else
+            System.out.println("Your floor is:" + floor + "Your Slot Id is:" + slotid);
+    }
+}
+class sedan extends fourWheelers{
+    int regno;
+    Date inTime;
+    int slotid;
+    void drive(){
+        if(slotid>30) {
+            System.out.println("This floor is full");
+        }else
+            System.out.println("Your floor is:" + floor + "Your Slot Id is:" + slotid);
+    }
+}
+class sUV extends fourWheelers{
+    int regno;
+    Date inTime;
+    int slotid;
+    void drive(){
+        if(slotid>30) {
+            System.out.println("This floor is full");
+        }else
+            System.out.println("Your floor is:" + floor + "Your Slot Id is:" + slotid);
+    }
+}
+class sixWheelers extends vehicle{
+    short floor;
+    void floor(short floor) {
+        this.floor=floor;
+        if (floor == 0){
+            System.out.println("You are on floor:" + floor + "This floor is for two wheeler.");
+        } else
+            System.out.println("You are on the wrong floor.");
+    }
+    int regno;
+    Date inTime;
+    int slotid;
+    void drive(){
+        if(slotid>30) {
+            System.out.println("This floor is full");
+        }else
+            System.out.println("Your floor is:" + floor + "Your Slot Id is:" + slotid);
+    }
+}
